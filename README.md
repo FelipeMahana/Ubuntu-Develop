11407e021decabe23fd34f1a692e7482bb3b7722
Felipe Mahana <f.mahana94@gmail.com>
2020-12-24 16:26:19 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Ubuntu Develop setup
+
+Este script esta pensado en la instalación básica de algunos programas de desarrollo y utilidades para el desarrollo.
+
+Es necesario tener instalado NVM, para el funcionamiento de este.
+
+Podrá elegir entre instalar Visual Studio Code o Atom como editor de texto, se explicara en [Instalación](#-Instalación)
+
+## Programas
+
+NodeJS,
+NPM,
+Nodemon,
+Visual Studo code o Atom.
+
+
+## Instalación
+
+Usa el siguiente comando para ejecutarlo fácilmente de una vez.
+
+Para Instalar una de los editores, debe agregar la variable al final del script, las opciones son vsc para Visual Studio Code o atom para Atom.
+Si no desea instalar ningún editor, deje en blanco.
+
+Normal:
+```bash
+wget https://raw.githubusercontent.com/FelipeMahana/Ubuntu-Develop/master/develop-ubuntu.sh && sudo chmod +x develop-ubuntu.sh && ./develop-ubuntu.sh
+```
+
+Visual Studio Code:
+```bash
+wget https://raw.githubusercontent.com/FelipeMahana/Ubuntu-Develop/master/develop-ubuntu.sh && sudo chmod +x develop-ubuntu.sh && ./develop-ubuntu.sh vsc
+```
+
+Atom:
+```bash
+wget https://raw.githubusercontent.com/FelipeMahana/Ubuntu-Develop/master/develop-ubuntu.sh && sudo chmod +x develop-ubuntu.sh && ./develop-ubuntu.sh atom
+```
+
+## Notas
+
+Este script no se a probado desde 0.
---
+++ develop-ubuntu.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#links
+# https://www.lifewire.com/pass-arguments-to-bash-script-2200571
+# https://linuxize.com/post/how-to-install-visual-studio-code-on-ubuntu-20-04/
+# https://linuxize.com/post/how-to-install-node-js-on-ubuntu-20-04/
+# https://linuxhandbook.com/if-else-bash/
+# https://gist.github.com/rachelhyman/b1f109155c9dafffe618
+
+
+# SCRIPT INSTALACIÓN HERRAMIENTAS TRABAJO PARA UBUNTU 20.04
+# es necesario tener instalado nvm
+
+cd ~
+sudo apt update -y
+
+# Instalación nvm
+#wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+# agregar nvm a plugins
+#sed -i 's/plugins=(git)/plugins=(git nvm)/g' ~/.zshrc
+
+# Instalación de NodeJS y NPM con nvm
+
+nvm install --lts
+
+# Instalacion de Nodemon con npm
+
+npm install -g nodemon
+
+# Elegir entre Atom o Visual Studio Code
+
+editor=$1
+
+if [ "$editor" = "vsc" ]; then
+    echo "Eligio $editor."
+	echo "Instalando Visual Studio Code."
+    # Visual Studio Code
+    #dependencias
+    sudo apt install software-properties-common apt-transport-https build-essential -y
+    # Instalación Visual Studio Code
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt install code
+elif [ "$editor" = "atom" ]; then
+	echo "Eligio $editor."
+    echo "Instalando Atom."
+    # Atom
+    wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+    sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+    sudo apt update && sudo apt install atom -y
+    #paquetes atom
+    apm install atom-beautify atom-ternjs emmet file-icons highlight-selected linter minimap pigments script minimap-git-diff minimap-highlight-selected
+else
+	echo "No se instalo ningun editor de texto."
+fi
+
+# Instalación Completada
+cd ~
+sudo apt update && sudo apt upgrade -y
+
+echo $'\n'$"*** Todo Listo, Reiniciar Computador. ***"
